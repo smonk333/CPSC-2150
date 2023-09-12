d2c7085cd93e067b0df23bec278e4ff4878dbae6
garrett <69862547+smonk333@users.noreply.github.com>
2023-09-12 13:30:03 -0400

    started the cpsc-2150 repo

---
+++ untitled/src/Receipt.java
@@ -0,0 +1,28 @@
+public class Receipt {
+
+    public Receipt (double taxRate) {
+
+    }
+
+    public void addToReceipt(double cost, int quantity, boolean isTaxable){
+
+    }
+
+    public double getNonTaxableSubtotal(){
+
+    }
+
+    public double getTaxableSubtotal(){
+
+    }
+
+    public double getTaxRate(){
+
+    }
+
+    public double computeTotal(){
+
+    }
+
+}
+
